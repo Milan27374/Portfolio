cc36242557caef1ce61fa6e41685f74833e8f4b3
Milan Kwanten <milankwanten@leerling.wico.be>
2025-02-24 15:10:14 +0100

    test

--- backend/models/project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProjectSchema = new mongoose.Schema({
-  naam: String,
-  category: String
-
-});
-
-module.exports = mongoose.model("Project", ProjectSchema,"project");
